c999000557cf371f1cdde7e571745bbbb4e3a5cb
amolbenare <amol.benare604@gmail.com>
2018-12-23 21:26:53 +0530

    first commit

---
+++ MQTT-master/app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://ec2-13-233-124-117.ap-south-1.compute.amazonaws.com:1883";
+
+    public static final String PUBLISH_TOPIC = "test";
+
+    public static final String CLIENT_ID = "androidkt";
+}
+
